11ead68fb370fc4b833201164130c09ce1470d7d
Ørjan Blom <blom@blom.tv>
2015-09-05 06:21:52 +0200

    Update plugins

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:plugins [[lein-exec "0.3.4"]
+{:user {:plugins [[lein-exec "0.3.5"]
                   [lein-pprint "1.1.1"]
-                  [venantius/ultra "0.3.3"]]
+                  [venantius/ultra "0.3.4"]]
         :ultra {:color-scheme :solarized_dark}}}
