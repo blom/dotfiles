17488e2750c2e026dd2d05368085eb3a17e4af6e
Ørjan Blom <blom@blom.tv>
2015-09-05 06:22:40 +0200

    Add eastwood

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,5 @@
-{:user {:plugins [[lein-exec "0.3.5"]
+{:user {:plugins [[jonase/eastwood "0.2.1"]
+                  [lein-exec "0.3.5"]
                   [lein-pprint "1.1.1"]
                   [venantius/ultra "0.3.4"]]
         :ultra {:color-scheme :solarized_dark}}}
