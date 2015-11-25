6de998a46b7b2b67f877895a67e378be1fb30a8b
Ørjan Blom <blom@blom.tv>
2015-11-25 21:29:31 +0100

    Update Leiningen plugins

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,5 @@
-{:user {:plugins [[jonase/eastwood "0.2.1"]
+{:user {:plugins [[jonase/eastwood "0.2.2"]
                   [lein-exec "0.3.5"]
-                  [lein-pprint "1.1.1"]
-                  [venantius/ultra "0.3.4"]]
+                  [lein-pprint "1.1.2"]
+                  [venantius/ultra "0.4.0"]]
         :ultra {:color-scheme :solarized_dark}}}
