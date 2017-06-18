51d7e52b17279b8940e253a2dcd8a04d50287e58
Ørjan Blom <blom@blom.tv>
2017-06-18 23:42:55 +0200

    Update profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,14 @@
-{:user {:plugins [[jonase/eastwood "0.2.2"]
-                  [lein-ancient "0.6.8"]
-                  [lein-exec "0.3.5"]
+{:user {:aliases {"slamhound" ["run" "-m" "slam.hound"]}
+        :dependencies [[cljfmt "0.5.6"]
+                       [slamhound "1.5.5"]]
+        :plugins [[jonase/eastwood "0.2.4"]
+                  [lein-ancient "0.6.10"]
+                  [lein-bikeshed "0.4.1"]
+                  [lein-cljfmt "0.5.6"]
+                  [lein-cloverage "1.0.9"]
+                  [lein-kibit "0.1.5"]
                   [lein-pprint "1.1.2"]
-                  [venantius/ultra "0.4.0"]]
-        :ultra {:color-scheme :solarized_dark}}}
+                  [lein-try "0.4.3"]
+                  [venantius/ultra "0.5.1"]
+                  [venantius/yagni "0.1.4"]]
+        :repl-options {:init (require 'cljfmt.core)}}}
